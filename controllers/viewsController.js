25f2aadcbda38488a23fbcf1e385b05e4b3662b8
Brian Crookshanks <zeron30@hotmail.com>
2022-06-11 11:07:22 -0700

    error rendering, login and logout

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,14 +13,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours,
+    tours
   });
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user',
+    fields: 'review rating user'
   });
 
   if (!tour) {
@@ -31,19 +31,19 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
-    tour,
+    tour
   });
 });
 
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
-    title: 'Log into your account',
+    title: 'Log into your account'
   });
 };
 
 exports.getAccount = (req, res) => {
   res.status(200).render('account', {
-    title: 'Your account',
+    title: 'Your account'
   });
 };
 
@@ -52,16 +52,16 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
     req.user.id,
     {
       name: req.body.name,
-      email: req.body.email,
+      email: req.body.email
     },
     {
       new: true,
-      runValidators: true,
+      runValidators: true
     }
   );
 
   res.status(200).render('account', {
     title: 'Your account',
-    user: updatedUser,
+    user: updatedUser
   });
 });
--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,37 +1,36 @@
 /* eslint-disable */
+export const displayMap = locations => {
+  const map = L.map('map', { zoomControl: false });
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-const map = L.map('map', { zoomControl: false });
+  var greenIcon = L.icon({
+    iconUrl: '/img/pin.png',
+    iconSize: [32, 40], // size of the icon
+    iconAnchor: [16, 45], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -50] // point from which the popup should open relative to the iconAnchor
+  });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+  const points = [];
+  locations.forEach(loc => {
+    // Create points
+    points.push([loc.coordinates[1], loc.coordinates[0]]);
 
-var greenIcon = L.icon({
-  iconUrl: '/img/pin.png',
-  iconSize: [32, 40], // size of the icon
-  iconAnchor: [16, 45], // point of the icon which will correspond to marker's location
-  popupAnchor: [0, -50], // point from which the popup should open relative to the iconAnchor
-});
+    // Add markers
+    L.marker([loc.coordinates[1], loc.coordinates[0]], { icon: greenIcon })
+      .addTo(map)
+      // Add popup
+      .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+        autoClose: false
+      })
+      .openPopup();
+  });
 
-const points = [];
-locations.forEach((loc) => {
-  // Create points
-  points.push([loc.coordinates[1], loc.coordinates[0]]);
+  const bounds = L.latLngBounds(points).pad(0.5);
+  map.fitBounds(bounds);
 
-  // Add markers
-  L.marker([loc.coordinates[1], loc.coordinates[0]], { icon: greenIcon })
-    .addTo(map)
-    // Add popup
-    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
-      autoClose: false,
-    })
-    .openPopup();
-});
-
-const bounds = L.latLngBounds(points).pad(0.5);
-map.fitBounds(bounds);
-
-map.scrollWheelZoom.disable();
+  map.scrollWheelZoom.disable();
+};
