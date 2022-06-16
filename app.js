b308ba8907af6b13e7175cea8bce58ae66bd5d8b
Brian Crookshanks <zeron30@hotmail.com>
2022-06-16 06:45:49 -0700

    stripe integration

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -158,6 +160,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
