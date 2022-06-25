0e2e74d5a8a53f3d915c6c1a5c088d31bc927607
Brian Crookshanks <zeron30@hotmail.com>
2022-06-25 08:36:26 -0700

    webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -72,6 +72,7 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].unit_amount / 100;
+  git;
   await Booking.create({
     tour,
     user,
